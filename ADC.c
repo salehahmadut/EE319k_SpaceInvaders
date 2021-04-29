466d3f94a9c1217c39126c0ad9e0c3b4dc43fc15
salehahmadut <73411066+salehahmadut@users.noreply.github.com>
2021-04-29 03:14:32 -0500

    Add files via upload

---
+++ ADC.c
@@ -0,0 +1,62 @@
+// ADC.c
+// Runs on TM4C123
+// Provide functions that initialize ADC0
+// Last Modified: 1/16/2021
+// Student names: change this to your names or look very silly
+// Last modification date: change this to the last modification date or look very silly
+
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+
+// ADC initialization function 
+// Input: none
+// Output: none
+// measures from PD2, analog channel 5
+void ADC_Init(uint32_t sac){  //uint32_t  sac
+	uint32_t delay;
+	SYSCTL_RCGCGPIO_R |= 0x08;
+	while((SYSCTL_PRGPIO_R&0x08) == 0){};
+	GPIO_PORTD_DIR_R &= ~0x04;
+	GPIO_PORTD_AFSEL_R |= 0x04;
+	GPIO_PORTD_DEN_R &= ~0x04;
+	GPIO_PORTD_AMSEL_R |= 0x04;
+	SYSCTL_RCGCADC_R |= 0x01;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	delay = SYSCTL_RCGCADC_R;
+	ADC0_SAC_R = sac;
+	ADC0_PC_R = 0x01;
+	ADC0_SSPRI_R = 0x0123;
+	ADC0_ACTSS_R &= ~0x0008;
+	ADC0_EMUX_R &= ~0xF000;
+	ADC0_SSMUX3_R = (ADC0_SSMUX3_R&0xFFFFFFF0)+5;
+	ADC0_SSCTL3_R = 0x0006;
+	ADC0_IM_R &= ~0x0008;
+	ADC0_ACTSS_R |= 0x0008;
+}
+
+//------------ADC_In------------
+// Busy-wait Analog to digital conversion
+// Input: none
+// Output: 12-bit result of ADC conversion
+// measures from PD2, analog channel 5
+uint32_t ADC_In(void){  
+	uint32_t data;
+	ADC0_PSSI_R = 0x0008;
+	while((ADC0_RIS_R&0x08)==0){};
+	data = ADC0_SSFIFO3_R&0xFFF;
+	ADC0_ISC_R = 0x0008;
+	return data;
+  
+}
+
+
---
+++ Switch.c
@@ -0,0 +1,27 @@
+// Switch.c
+// This software to input from switches or buttons
+// Runs on TM4C123
+// Program written by: put your names here
+// Date Created: 3/6/17 
+// Last Modified: 1/14/21
+// Lab number: 10
+// Hardware connections
+// TO STUDENTS "REMOVE THIS LINE AND SPECIFY YOUR HARDWARE********
+
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+// Code files contain the actual implemenation for public functions
+// this file also contains an private functions and private data
+
+void Switch_Init(void){
+	SYSCTL_RCGCGPIO_R |= 0x10;
+	while((SYSCTL_PRGPIO_R&0x10) == 0){};
+	GPIO_PORTE_DEN_R |= 0x01;
+	GPIO_PORTE_DIR_R &= 0xF0;
+	
+}
+
+uint32_t Switch_In(void){
+	return GPIO_PORTE_DATA_R&0x01;
+}
+
